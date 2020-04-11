8be4000b41162f7957c362040b049ea5ef6131e0
Herbert Kagumba <habukagumba@gmail.com>
2020-04-11 17:45:30 +0300

    Initialise Zabo SDK when loading index route
    - Use index route to initialize Zabo SDK and do only once
    - Add initialized zabo instance to app locals for other requests

--- routes/index.js
+++ routes/index.js
@@ -1,72 +1,77 @@
 const express = require('express')
 const router = express.Router()
-
 // Import Zabo SDK
 const Zabo = require('zabo-sdk-js')
 
-// Initializing the JS SDK
-// Docs: https://zabo.com/docs/#initializing-the-js-sdks
-Zabo.init({
-  apiKey: process.env.PUBLIC_API_KEY,
-  secretKey: process.env.SECRET_API_KEY,
-  env: 'sandbox'
-})
-  .then(zabo => {
-    let myUser = null
+router.post('/accounts', async (req, res) => {
+  let myUser
+  const zabo = req.app.locals.zabo
+  const account = req.body
 
-    router.post('/accounts', async (req, res) => {
-      const account = req.body
-
+  try {
+    if (!myUser) {
+      // Create a User
+      // Docs: https://zabo.com/docs/#create-a-user
+      myUser = await zabo.users.create(account)
+    } else {
+      // Add Account
+      // Docs: https://zabo.com/docs/#add-account-to-existing-user
       try {
-        if (!myUser) {
-          // Create a User
-          // Docs: https://zabo.com/docs/#create-a-user
-          myUser = await zabo.users.create(account)
-        } else {
-          // Add Account
-          // Docs: https://zabo.com/docs/#add-account-to-existing-user
-          try {
-            await zabo.users.addAccount(myUser, account)
-            myUser.accounts.push(account)
-          } catch (e) {
-            if (!e.message.includes("already belongs")) {
-              console.error(e)
-            }
-            // Else do nothing because the account already belongs to the user.
-          }
+        await zabo.users.addAccount(myUser, account)
+        myUser.accounts.push(account)
+      } catch (e) {
+        if (!e.message.includes("already belongs")) {
+          console.error(e)
         }
-
-        // Get Transactions
-        // Docs: https://zabo.com/docs/#get-transaction-history
-        const transactions = await zabo.transactions.getList({
-          userId: myUser.id,
-          accountId: account.id
-        })
-
-        // Get Balances
-        // Docs: https://zabo.com/docs/#get-a-specific-balance
-        const balances = await zabo.users.getBalances({
-          userId: myUser.id,
-          accountId: account.id,
-          currencies: ['ALL']
-        })
-
-        res.send({
-          accounts: myUser.accounts,
-          balances: balances.data,
-          transactions: transactions
-        })
-      } catch (err) {
-        res.status(500).send({ message: err.message })
+        // Else do nothing because the account already belongs to the user.
       }
+    }
+
+    // Get Transactions
+    // Docs: https://zabo.com/docs/#get-transaction-history
+    const transactions = await zabo.transactions.getList({
+      userId: myUser.id,
+      accountId: account.id
     })
 
-    router.get('/', function (req, res, next) {
-      res.render('index', {
-        title: 'Zabo Node.js Quickstart App',
-        clientId: process.env.CLIENT_ID
-      })
+    // Get Balances
+    // Docs: https://zabo.com/docs/#get-a-specific-balance
+    const balances = await zabo.users.getBalances({
+      userId: myUser.id,
+      accountId: account.id,
+      currencies: ['ALL']
     })
-  })
+
+    res.send({
+      accounts: myUser.accounts,
+      balances: balances.data,
+      transactions: transactions
+    })
+  } catch (err) {
+    res.status(500).send({ message: err.message })
+  }
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    if (!Boolean(req.app.locals.zabo)) {
+      // Initializing the JS SDK
+      // Docs: https://zabo.com/docs/#initializing-the-js-sdks
+      const zabo = await Zabo.init({
+        apiKey: process.env.PUBLIC_API_KEY,
+        secretKey: process.env.SECRET_API_KEY,
+        env: 'sandbox'
+      })
+      req.app.locals.zabo = zabo
+    }
+
+    res.render('index', {
+      title: 'Zabo Node.js Quickstart App',
+      clientId: process.env.CLIENT_ID
+    })
+  } catch (error) {
+    res.status(500).send({ message: err.message })
+  }
+})
 
 module.exports = router
